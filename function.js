1f503dd8508c3047812d317ad1bf1215369fdabe
joestortuguitas <125600689+joestortuguitas@users.noreply.github.com>
2023-02-22 13:05:28 -0300

    Add files via upload
    responsive terminado. faltan detalles con fuentes y el problema del pointer en todo el documento. + los links a redes sociales

--- function.js
+++ function.js
@@ -12,3 +12,10 @@ function mostrarOcultarMenu(){
 	}
 }
 //para ocultar el menu una vez seleccionado
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x <links.length;x++){
+	links[x].onclick = function(){
+		menu.style.display = "none";
+		menu_visible = false;
+	}
+}
